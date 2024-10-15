8b3b46159151b4d180f9a1ae8eead7d73a76399e
laurensaggar <saggar.4@osu.edu>
2024-10-14 21:30:19 -0400

    Initial Commit

---
+++ BeanSpotted/BeanSpottedApp.swift
@@ -0,0 +1,17 @@
+//
+//  BeanSpottedApp.swift
+//  BeanSpotted
+//
+//  Created by Lauren Saggar on 10/14/24.
+//
+
+import SwiftUI
+
+@main
+struct BeanSpottedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
