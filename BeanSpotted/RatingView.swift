1e2cbd412e95388578330d6d7de721d306d00d78
laurensaggar <saggar.4@osu.edu>
2024-10-23 18:32:13 -0400

    Added star rating and review attributes to review

---
+++ BeanSpotted/RatingView.swift
@@ -0,0 +1,18 @@
+//
+//  RatingView.swift
+//  BeanSpotted
+//
+//  Created by Lauren Saggar on 10/23/24.
+//
+
+import SwiftUI
+
+struct RatingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RatingView()
+}
