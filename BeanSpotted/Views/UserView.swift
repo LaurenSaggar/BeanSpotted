7045cbc6405d5934c796fbf73789f2d3235f9d45
laurensaggar <saggar.4@osu.edu>
2025-04-08 23:28:25 -0600

    Commented out user and added multi-select filter

---
+++ BeanSpotted/Views/UserView.swift
@@ -0,0 +1,33 @@
+//
+//  UserView.swift
+//  BeanSpotted
+//
+//  Created by Lauren Saggar on 11/21/24.
+//
+
+//import SwiftUI
+//import SwiftData
+//
+//struct UserView: View {
+//    @Environment(\.modelContext) var modelContext
+//    @Environment(\.dismiss) var dismiss
+//    @Query var coffeeShops: [CoffeeShop]
+//    @Query var users: [User]
+//    
+//    // User variables
+//    @State private var name = ""
+//    @State private var bio = ""
+//    
+//    var body: some View {
+//        NavigationStack {
+//            Form {
+//                Text(users[0].name)
+//                Text(users[0].bio ?? "")
+//            }
+//        }
+//    }
+//}
+//
+//#Preview {
+//    UserView()
+//}
